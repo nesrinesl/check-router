9dc949d098306d515c9d85d69ee5978ee5b75bd3
nesrinesl <slama9082@gmail.com>
2024-02-06 00:37:22 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import MovieDetails from "./components/pages/MovieDetails";
 import ErrorPage from "./components/pages/ErrorPage";
 import ListMovies from "./components/pages/ListMovies";
 import Home from "./components/pages/Home";
-
+import React, { useState } from 'react';
+import GoogleMapComponent from './GoogleMapComponent';
 function App() {
   const [movies, setMovies] = useState(moviesData);
   const [inputSearch, setInputSearch] = useState("");
-
+  const [zoomLevel, setZoomLevel] = useState(12); // Set initial zoom level
+  const [mapCenter, setMapCenter] = useState({ lat: 37.7749, lng: -122.4194 })
   const add = (newMovie) => {
     setMovies([...movies, newMovie]);
   };
@@ -33,7 +35,12 @@ function App() {
       <Route path='/movies' element={ <>
         <AddMovie add={add} />
         <FilterMovie newRate={newRate} setNewRate={setNewRate} setInputSearch={setInputSearch} />
-
+        <GoogleMapComponent
+        zoomLevel={zoomLevel}
+        mapCenter={mapCenter}
+        onZoomChange={setZoomLevel}
+        onMapClick={setMapCenter}
+      />
       
         <ListMovies  movies={movies} inputSearch={inputSearch} newRate={newRate}/>
         </>} /> 
